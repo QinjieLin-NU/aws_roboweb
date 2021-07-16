48a2e580830b514a0dcac33457e63c8395ac6314
Tony Luo <tonyluozn@gmail.com>
2021-07-16 14:54:14 +0800

    update progress chart

--- src/Pages/RobotView/Components/bottomBar.js
+++ src/Pages/RobotView/Components/bottomBar.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from "react";
-import { Button, ButtonGroup, ClickAwayListener } from "@material-ui/core";
+import { CircularProgress } from "@material-ui/core";
 import { Map, Image, MoreHoriz } from "@material-ui/icons"
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -19,19 +19,26 @@ const useStyles = makeStyles(theme => ({
         height: "30px",
         borderRadius: "15px",
         opacity: "0.6",
-        textAlign: "center"
+        alignItems: "center",
+        justifyContent:"center"
     },
     item: {
-        paddingTop:"5px",
-        flex: "10%",
+//       paddingTop:"5px",
+ //       flex: "8%",
         fontSize: "12px",
-        fontWeight: "bold"
+        fontWeight: "bold",
+        margin:"7px",
     },
     text: {
         color: theme.palette.text.primary
     },
     number: {
         color: "#FFFFFF"
+    },
+    progress:{
+        color:"#18D2FF",
+        paddingTop:"6px",
+        margin:"2px",
     }
 
 
@@ -42,12 +49,16 @@ function BottomBar(props) {
     const classes = useStyles();
     return (
         <div className={classes.wrapper}>
+            <CircularProgress className={classes.progress} variant="determinate" size={16} value={29} />
             <span className={classes.item + " " + classes.text}>CPU</span>
             <span className={classes.item + " " + classes.number}>29%</span>
+            <CircularProgress className={classes.progress} variant="determinate" size={16} value={12} />
             <span className={classes.item + " " + classes.text}>Memory</span>
             <span className={classes.item + " " + classes.number}>12%</span>
+            <CircularProgress className={classes.progress} variant="determinate" size={16} value={61} />
             <span className={classes.item + " " + classes.text}>Battery</span>
             <span className={classes.item + " " + classes.number}>61%</span>
+            <CircularProgress className={classes.progress} variant="determinate" size={16} value={90} />
             <span className={classes.item + " " + classes.text}>PING</span>
             <span className={classes.item + " " + classes.number}>68ms</span>
         </div>
